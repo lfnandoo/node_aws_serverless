bb1b54479054b26b71c51f4e61fe040ef428d619
lfnandoo <devfernandoof@gmail.com>
2020-12-10 21:36:45 -0300

    last

--- src/handlers/secrets.js
+++ src/handlers/secrets.js
@@ -4,7 +4,10 @@ const { v4: uuidv4 } = require('uuid')
 
 const SecretModel = require('../resources/db/models/Secret')
 
-module.exports.create = async (event) => {
+module.exports.create = async (event, context) => {
+  // good practice for lambda functions in AWS
+  context.callbackWaitsForEmptyEventLoop = false
+
   const { name, email } = JSON.parse(event.body)
 
   const externalId = uuidv4()
@@ -17,6 +20,15 @@ module.exports.create = async (event) => {
       externalId,
       adminKey
     })
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        success: true,
+        id: externalId,
+        adminKey
+      })
+    }
   } catch (err) {
     return {
       statusCode: 500,
--- src/resources/db/connection.js
+++ src/resources/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 let connection = null
 
 const URI =
-  'mongodb+srv://secret:7APb6aztL2JMwqQ4@cluster0.2pdpa.mongodb.net/secret?retryWrites=true&w=majority'
+  'mongodb+srv://secret:6dR5sa50u5p9kawZ@cluster0.2pdpa.mongodb.net/secret?retryWrites=true&w=majority'
 
 module.exports = async () => {
   if (!connection) {
